789f029509fbff7e2f416a4916c2629aa7b7ae99
Filipe Bomfim <lipe.bomfim99@gmail.com>
2023-04-05 09:53:38 -0300

    Correções para primeira versão do projeto

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,27 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import {useLocation} from 'react-router-dom'
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
-import StarIcon from '@mui/icons-material/StarBorder';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
 import GlobalStyles from '@mui/material/GlobalStyles';
 import Container from '@mui/material/Container';
 import Title from '../../components/texts/Title';
 import GreatWord from '../../components/texts/GreatWord';
 import Subtitle from '../../components/texts/Subtitle';
-import { Alert, Divider } from '@mui/material';
 import AlertMessage from '../../components/alert/AlertMessage';
 import LinkButton from '../../components/buttons/LinkButton';
-import { width } from '@mui/system';
 import ProjectCard from '../../components/cards/ProjectCard';
 import Loading from '../../components/loading/Loading';
 
